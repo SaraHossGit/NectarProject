30e031872a96ca35e728f634aca088a9bcf1f1fb
DELL <s.hossam@nu.edu.eg>
2022-09-27 11:04:01 +0200

    Recycler Views Update

--- app/src/main/java/com/example/nectarproject/HomeScreen.java
+++ app/src/main/java/com/example/nectarproject/HomeScreen.java
@@ -15,14 +15,28 @@ public class HomeScreen extends AppCompatActivity {
     RecyclerView recyclerView1, recyclerView2, recyclerView3, recyclerView4;
     ProductAdapter productAdapter1, productAdapter2, productAdapter3;
     GroceriesAdapter groceriesAdapter;
-    ArrayList <ProductModel> productList = new ArrayList<>();
+    ArrayList <ProductModel> productList1 = new ArrayList<>();
+    ArrayList <ProductModel> productList2 = new ArrayList<>();
+    ArrayList <ProductModel> productList3 = new ArrayList<>();
     ArrayList <GroceriesModel> groceryList = new ArrayList<>();
 
-    // Product Data
-    int[] productImage = {R.drawable.item1, R.drawable.item2, R.drawable.item3, R.drawable.item4};
-    String[] productName = {"Organic Bananas", "Bell Pepper", "Ginger", "Meat"};
-    String[] productAmount = {"7pcs, Priceg", "7pcs, Priceg", "7pcs, Priceg", "7pcs, Priceg"};
-    String[] productPrice = {"$4.99", "$4.99", "$4.99", "$4.99"};
+    // Exclusive Product Data
+    int[] productImage1 = {R.drawable.item1, R.drawable.item2, R.drawable.item3};
+    String[] productName1 = {"Organic Bananas", "Bell Pepper", "Ginger"};
+    String[] productAmount1 = {"7pcs, Priceg", "7pcs, Priceg", "7pcs, Priceg"};
+    String[] productPrice1 = {"$4.99", "$4.99", "$4.99"};
+
+    // Best Selling Product Data
+    int[] productImage2 = {R.drawable.item4, R.drawable.item5};
+    String[] productName2 = {"Meat", "Chicken"};
+    String[] productAmount2 = {"1Kg, Priceg", "1Kg, Priceg"};
+    String[] productPrice2 = {"$40.00", "$35.99"};
+
+    // Last Product Data
+    int[] productImage3 = {R.drawable.item1, R.drawable.item2, R.drawable.item3, R.drawable.item4};
+    String[] productName3 = {"Organic Bananas", "Bell Pepper", "Ginger", "Meat"};
+    String[] productAmount3 = {"7pcs, Priceg", "7pcs, Priceg", "7pcs, Priceg", "7pcs, Priceg"};
+    String[] productPrice3 = {"$4.99", "$4.99", "$4.99", "$4.99"};
 
     // Groceries Data
     int[] groceryImage = {R.drawable.pulses, R.drawable.rice};
@@ -35,21 +49,23 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
 
         recyclerView1 = findViewById(R.id.exclusive_recycler_view);
-        productAdapter1 = new ProductAdapter(this, productList);
+        productAdapter1 = new ProductAdapter(this, productList1);
         recyclerView1.setAdapter(productAdapter1);
         recyclerView1.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        setProducts(1);
 
         recyclerView2 = findViewById(R.id.best_selling_recycler_view);
-        productAdapter2 = new ProductAdapter(this, productList);
+        productAdapter2 = new ProductAdapter(this, productList2);
         recyclerView2.setAdapter(productAdapter2);
         recyclerView2.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        setProducts(2);
 
         recyclerView3 = findViewById(R.id.last_recycler_view);
-        productAdapter3 = new ProductAdapter(this, productList);
+        productAdapter3 = new ProductAdapter(this, productList3);
         recyclerView3.setAdapter(productAdapter3);
         recyclerView3.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
-        setProducts();
+        setProducts(3);
 
         recyclerView4 = findViewById(R.id.groceries_recycler_view);
         groceriesAdapter = new GroceriesAdapter(this, groceryList);
@@ -70,14 +86,40 @@ public class HomeScreen extends AppCompatActivity {
         }
     }
 
-    private void setProducts() {
-        for (int i = 0; i < productName.length;i++){
-            productList.add(new ProductModel(
-                    productImage[i],
-                    productName[i],
-                    productAmount[i],
-                    productPrice[i]
-            ));
-        }
+    private void setProducts(int listNum) {
+            switch (listNum){
+                case 1:
+                    for (int i = 0; i < productName1.length;i++) {
+                        productList1.add(new ProductModel(
+                                productImage1[i],
+                                productName1[i],
+                                productAmount1[i],
+                                productPrice1[i]
+                        ));
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < productName2.length;i++) {
+                        productList2.add(new ProductModel(
+                                productImage2[i],
+                                productName2[i],
+                                productAmount2[i],
+                                productPrice2[i]
+                        ));
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i < productName3.length;i++) {
+                        productList3.add(new ProductModel(
+                                productImage3[i],
+                                productName3[i],
+                                productAmount3[i],
+                                productPrice3[i]
+                        ));
+                    }
+                    break;
+            }
+
+
     }
 }
